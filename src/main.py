fc53812870a9475e1c71bb0a09d2b9b32a70f264
SolisAlexis24 <ianalexis9137@gmail.com>
2025-10-14 17:24:35 -0600

    Actividad 4

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import lcd
 from time import sleep
 
 def main():
+	global LCD
 	LCD = lcd.display(lcd.LCD_ADDR, lcd.I2C_BUS)
 	# Imprime nombres
 	LCD.setCursor(0,0)
@@ -12,11 +13,21 @@ def main():
 	# Imprime unidades (Estaticas)
 	LCD.setCursor(5,1)
 	LCD.printTempUnits(lcd.CELSIUS)
+	LCD.setCursor(13,1)
+	LCD.printTempUnits(lcd.FAHRENHEIT)
 	# Imprime temperatura (Dinamicas)
 	while True:
-		LCD.setCursor(0,1)
-		LCD.printText(format(get_temperature(), ".2f"))
-		sleep(1)
+		try:
+			temp = get_temperature()
+			LCD.setCursor(0,1)
+			LCD.printText(format(temp, ".2f"))
+			LCD.setCursor(8,1)
+			LCD.printText(format(temp*9/5 + 32, ".2f"))
+			sleep(1)
+		except KeyboardInterrupt:
+			LCD.clear()
+			print("\n")
+			return
 
 
 if __name__ == '__main__':
